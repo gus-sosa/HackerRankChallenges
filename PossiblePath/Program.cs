a636cb4528c4b806ea52c9e589d1b1bfa8cd2904
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-01 23:26:52 -0600

    Hacker Rank Solutions

--- PossiblePath/Program.cs
+++ PossiblePath/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+public class Solution {
+    public static void Main(string[] args) {
+        long a = 0, b = 0, x = 0, y = 0;
+        int n = int.Parse(Console.ReadLine());
+        string[] arr = new string[4];
+        while (n-- != 0) {
+            arr = Console.ReadLine().Split(' ');
+            a = long.Parse(arr[0]);
+            b = long.Parse(arr[1]);
+            x = long.Parse(arr[2]);
+            y = long.Parse(arr[3]);
 
-namespace PossiblePath {
-    class Program {
-        static void Main(string[] args) {
+            Console.WriteLine(Math.Abs(x - a) % b == 0 && (b == y || Math.Abs(y - a) % b == 0) ? "YES" : "NO");
         }
     }
 }
