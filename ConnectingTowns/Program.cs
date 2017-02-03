9b54f42b2961c686384a4181ccc931b209015762
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-03 09:22:46 -0600

    Solution to Connecting Towns

---
+++ ConnectingTowns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace ConnectingTowns {
+    class Program {
+        static void Main(string[] args) {
+            int t = int.Parse(Console.ReadLine()), n = 0;
+            int m = 1234567;
+            while (t-- != 0) {
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine(Console.ReadLine()
+                                    .Split(' ')
+                                    .Select(v => int.Parse(v))
+                                    .Aggregate((current, aggre) => current * aggre % m));
+            }
+        }
+    }
+}
