97b524275aaa746aea7653739ae52a412e552e40
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-12 01:51:16 -0600

    Solution to hacker rank in a string

---
+++ HackerRank in a String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Solution {
+    static void Main(String[] args) {
+        int q = Convert.ToInt32(Console.ReadLine());
+        int indexWord = 0;
+        string word = "hackerrank";
+        while (q-- > 0) {
+            string s = Console.ReadLine();
+            indexWord = 0;
+            for (int i = 0; i < s.Length && indexWord < word.Length; i++) {
+                if (word[indexWord] == s[i]) {
+                    indexWord++;
+                }
+            }
+            Console.WriteLine(indexWord == word.Length ? "YES" : "NO");
+        }
+    }
+}
