50a53b6bdbeca2fdc299cfd5c871bdf72d7fca0c
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-12 01:43:28 -0600

    Solution to Minimum absolute difference in an array

---
+++ Minimum Absolute Difference in an Array/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Solution {
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+        Array.Sort(a);
+        int minDiff = int.MaxValue;
+        for (int i = 0; minDiff != 0 && i < a.Length - 1; i++) {
+            minDiff = Math.Min(minDiff, Math.Abs(a[i] - a[i + 1]));
+        }
+        Console.WriteLine(minDiff);
+    }
+}
