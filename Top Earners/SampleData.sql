7f7f60cb6cc83ab0652d88f20432f6e49cca93b7
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-15 02:37:29 -0600

    sample data

---
+++ Top Earners/SampleData.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO Employee (employee_id,name,months,salary) VALUES
+(12228,'Rose',15,1968),
+(33645,'Angela',1,3443),
+(45692,'Frank',17,1608),
+(56118,'Patrick',7,1345),
+(59725,'Lisa',11,2330),
+(74197,'Kimberly',16,4372),
+(78454,'Bonnie',8,1771),
+(83565,'Michael',6,2017),
+(98607,'Todd',5,3396),
+(99989,'Joe',9,3573);
