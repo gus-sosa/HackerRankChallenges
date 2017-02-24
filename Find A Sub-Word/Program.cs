afe64ec7f616b2415acfef16356fa5f5ef316d42
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-24 16:42:36 -0600

    Solution added

---
+++ Find A Sub-Word/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Find_A_Sub_Word {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            List<string> sentences = new List<string>();
+            while (n-- > 0) {
+                sentences.Add(Console.ReadLine());
+            }
+            int q = int.Parse(Console.ReadLine());
+            while (q-- > 0) {
+                string input = Console.ReadLine();
+                n = 0;
+                var regex = new Regex($"[_0-9a-zA-Z]{input}[_0-9a-zA-Z]");
+                foreach (var sentence in sentences) {
+                    n += regex.Matches(sentence).Count;
+                }
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
