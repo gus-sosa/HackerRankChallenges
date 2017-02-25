c858e676d61943fb167d6d68537bac9620362084
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-25 16:08:14 -0600

    Solution

---
+++ DetectValidLatLong/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DetectValidLatLong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var regex = new Regex(@"^\([-\+]?([0-9](\.[0-9]+)?|[1-8][0-9](\.[0-9]+)?|90(\.0+)?), [-\+]?([0-9](\.[0-9]+)?|[1-9][0-9](\.[0-9]+)?|1[0-7][0-9](\.[0-9]+)?|180(\.0+)?)\)$");
+            while (n-- > 0)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(regex.IsMatch(input) ? "Valid" : "Invalid");
+            }
+        }
+    }
+}
