fde5bb99e9de295d544fd2ec2c2d3f6a6beb0fb8
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-25 16:19:44 -0600

    Solution

---
+++ HackerRank Tweets/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Tweets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            var regex = new Regex("[hH][aA][cC][kK][eE][rR]{2}[aA][nN][kK]");
+            while (n-- > 0)
+            {
+                string input = Console.ReadLine();
+                count += regex.Matches(input).Count;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
