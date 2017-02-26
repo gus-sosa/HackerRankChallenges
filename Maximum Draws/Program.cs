62ac0f97463163a796a9266bdbf76435ac99798d
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-26 01:31:35 -0600

    Solution

---
+++ Maximum Draws/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Maximum_Draws
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(n + 1);
+            }
+        }
+    }
+}
