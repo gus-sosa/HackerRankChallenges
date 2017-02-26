745fd61333fe317f4ef1cdcc5a852094f620eaa3
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-26 12:54:53 -0600

    Solution

---
+++ Utopian Identification Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Utopian_Identification_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"^[a-z]{0,3}\d{2,8}[A-Z]{3,}$");
+            int n = int.Parse(Console.ReadLine());
+            while (n-- > 0)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(regex.IsMatch(input) ? "VALID" : "INVALID");
+            }
+        }
+    }
+}
