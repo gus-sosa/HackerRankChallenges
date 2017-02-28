f612efe3404633f6698d742f1ddbb169338683a1
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-28 14:55:00 -0600

    Solution

---
+++ Saying Hi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Saying_Hi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"^[Hh][Ii]\s[^Dd]");
+            int n = int.Parse(Console.ReadLine());
+            while (n-- > 0)
+            {
+                string input = Console.ReadLine();
+                if (regex.IsMatch(input))
+                    Console.WriteLine(input);
+            }
+        }
+    }
+}
