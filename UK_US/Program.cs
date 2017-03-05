cd882d9a7b6dfb18b09aceab48b823ff12e1c6ac
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-03-04 20:27:00 -0600

    Solution

---
+++ UK_US/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UK_US
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input;
+            var builder = new StringBuilder();
+            while (n-- > 0)
+            {
+                builder.AppendLine(Console.ReadLine());
+            }
+            input = builder.ToString();
+            n = int.Parse(Console.ReadLine());
+            var regex = new Regex("our");
+            while (n-- > 0)
+            {
+                string britishWord = Console.ReadLine();
+                string regexAmerican = regex.Split(britishWord).Aggregate((agg, i) => $"{agg}or{i}");
+                Console.WriteLine(new Regex($@"\b({britishWord}|{regexAmerican})\b").Matches(input).Count);
+            }
+        }
+    }
+}
