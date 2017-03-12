077f895f7c197bff73f444e1e6e5d48b144d9801
Unknown <g.sosacuervo1@gmail.com>
2017-03-12 12:56:29 -0500

    The right solution to Placements problem
    What happen if a person have more than two friend with a greater salary. This solution solves this problem.

--- Placements/Solution.sql
+++ Placements/Solution.sql
@@ -1,7 +1,9 @@
-﻿SELECT s.Name
+﻿SELECT MAX(s.Name)
 FROM Students s
 JOIN Packages p1 ON s.Id=p1.Id
 JOIN Friends f ON s.Id=f.Id
 JOIN Packages p2 ON f.Friend_ID=p2.Id
 WHERE p1.Salary<p2.Salary
-ORDER BY p2.Salary;
+GROUP BY s.Id
+HAVING MAX(p1.Salary)<MAX(p2.Salary)
+ORDER BY MAX(p2.Salary);
