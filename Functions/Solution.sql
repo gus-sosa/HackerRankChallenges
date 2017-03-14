78a2fb5c8ec638cfcf7f83f21e1d56050343522d
Unknown <g.sosacuervo1@gmail.com>
2017-03-14 13:46:43 -0500

    Solution

---
+++ Functions/Solution.sql
@@ -0,0 +1,5 @@
+﻿SELECT DISTINCT f.X,f.Y
+FROM Functions f
+WHERE (f.X=f.Y AND (SELECT COUNT(*) FROM Functions f1 WHERE f1.X=f1.Y AND f1.X=f.X)>1) OR 
+	  (f.X<f.Y AND (SELECT COUNT(*) FROM Functions f1 WHERE f.X=f1.Y AND f.Y=f1.X)>0)
+ORDER BY f.X;
