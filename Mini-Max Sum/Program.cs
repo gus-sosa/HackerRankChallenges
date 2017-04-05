73283f29f2cc6f817361873752b0384b5c9df345
Unknown <g.sosacuervo1@gmail.com>
2017-04-05 01:17:04 -0500

    Solution to problem Mini-Max Sum

---
+++ Mini-Max Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string[] tokens = Console.ReadLine().Split(' ');
+        int[] nums = Array.ConvertAll(tokens, Convert.ToInt32);
+        long sum = 0, minSum = long.MaxValue, maxSum = -1;
+        for (int i = 0; i < nums.Length; i++)
+            sum += nums[i];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            long tmpSum = sum - nums[i];
+            minSum = Math.Min(minSum, tmpSum);
+            maxSum = Math.Max(maxSum, tmpSum);
+        }
+
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+}
