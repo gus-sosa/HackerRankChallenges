b8d689e35063d95f5ed0d7203e0c9a8b28329280
Unknown <g.sosacuervo1@gmail.com>
2017-04-05 01:39:44 -0500

    Solution to problem Time Conversion

---
+++ Time Conversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string time = Console.ReadLine();
+        bool am = time.Contains('A');
+        string colon_mmss_am = new Regex(@":\d{2}:\d{2}").Match(time).Value;
+        int hh = int.Parse(new Regex(@"\d{2}").Matches(time)[0].Value);
+        if (am)
+        {
+            if (hh == 12)
+                hh = 0;
+        }
+        else
+        {
+            if (hh != 12)
+                hh += 12;
+        }
+        string hh_string = hh.ToString();
+        if (hh_string.Length == 1)
+            hh_string = "0" + hh_string;
+        Console.WriteLine($"{hh_string}{colon_mmss_am}");
+    }
+}
