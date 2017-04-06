0fdf3a56c76da04d94060997d52105a130109598
Unknown <g.sosacuervo1@gmail.com>
2017-04-06 14:12:19 -0500

    Solution to problem The Power Sum
    https://www.hackerrank.com/challenges/the-power-sum

---
+++ ThePowerSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Solution
+{
+    static int N;
+
+    static void Main(String[] args)
+    {
+        int X = int.Parse(Console.ReadLine());
+        N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Cant(X, int.MaxValue));
+    }
+
+    private static int Cant(int num, int min)
+    {
+        if (num == 0)
+            return 1;
+
+        int cant = 0;
+        for (int i = Math.Min(min, Convert.ToInt32(Math.Floor(Math.Pow(num, 1 / (N * 1f))))); i > 0; i--)
+            cant += Cant(Convert.ToInt32(num - Math.Pow(i, N)), i - 1);
+
+        return cant;
+    }
+}
