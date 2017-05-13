7fed2d999df8ce4bd84d951a5db568b588ac1726
Gustavo Sosa <gcuervo@rdisoftware.com>
2017-05-13 00:18:24 -0500

    Temporal solution
    It may work

--- MatrixLayerRotation/Program.cs
+++ MatrixLayerRotation/Program.cs
@@ -8,38 +8,6 @@ namespace MatrixLayerRotation
 {
     class Program
     {
-        static int[,] Mirror(int[,] m)
-        {
-            int[,] result = new int[m.GetLength(0), m.GetLength(1)];
-            for (int i = 0; i < m.GetLength(0); i++)
-                for (int j = 0; j < m.GetLength(1); j++)
-                    result[m.GetLength(0) - i - 1, m.GetLength(1) - j - 1] = m[i, j];
-            return result;
-        }
-
-        static int[,] RightRotation(int[,] m)
-        {
-            int[,] result = new int[m.GetLength(1), m.GetLength(0)];
-            for (int i = 0; i < m.GetLength(0); i++)
-                for (int j = 0; j < m.GetLength(1); j++)
-                    result[j, m.GetLength(1) - i - 1] = m[i, j];
-            return result;
-        }
-
-        static int[,] LeftRotation(int[,] m)
-        {
-            int[,] result = new int[m.GetLength(1), m.GetLength(0)];
-            for (int i = 0; i < m.GetLength(0); i++)
-                for (int j = 0; j < m.GetLength(1); j++)
-                    result[m.GetLength(1) - j - 1, i] = m[i, j];
-            return result;
-        }
-
-        static int[,] NoRotation(int[,] m)
-        {
-            return m;
-        }
-
         static void Main(string[] args)
         {
             int m, n, r;
@@ -48,7 +16,6 @@ namespace MatrixLayerRotation
             n = Convert.ToInt32(tokens[1]);
             r = Convert.ToInt32(tokens[2]);
             int[,] matrix = new int[m, n];
-
             for (int i = 0; i < m; i++)
             {
                 tokens = Console.ReadLine().Split();
@@ -56,32 +23,85 @@ namespace MatrixLayerRotation
                     matrix[i, j] = Convert.ToInt32(tokens[j]);
             }
 
-            Func<int[,], int[,]> RotationFunc;
-            switch (r % 4)
+            Print(GetMatrixRotated(matrix, r));
+        }
+
+        private static int[,] GetMatrixRotated(int[,] matrix, int r)
+        {
+            int m = matrix.GetLength(0), n = matrix.GetLength(1);
+            int[,] result = new int[m, n];
+
+            for (int numLayers = Math.Min(m, n) / 2, numLayerSkiped = 0; numLayerSkiped < numLayers; numLayerSkiped++)
+                RotateLayer(numLayerSkiped, matrix, r, result);
+
+            return result;
+        }
+
+        private static void RotateLayer(int numLayerSkiped, int[,] matrix, int r, int[,] result)
+        {
+            int m = matrix.GetLength(0), n = matrix.GetLength(1);
+            int width = n - 2 * numLayerSkiped, height = m - 2 * numLayerSkiped;
+            int totalPositions = 2 * (width + height) - 4;
+            int rot = r % totalPositions;
+            int row, col;
+
+            //rotating first row
+            row = height - 1;
+            col = width - 1;
+            for (int i = 0; i < width; i++, col++)
             {
-                case 1:
-                    RotationFunc = RightRotation;
-                    break;
-
-                case 2:
-                    RotationFunc = Mirror;
-                    break;
-
-                case 3:
-                    RotationFunc = LeftRotation;
-                    break;
-
-                case 0:
-                default:
-                    RotationFunc = NoRotation;
-                    break;
+                int currentValue = matrix[row, col];
+                int x, y;
+                GetCoords(width, height, row, col, rot, out x, out y);
+                result[x + height - 1, y + width - 1] = currentValue;
             }
 
-            int[,] result = RotationFunc(matrix);
-            for (int i = 0; i < result.GetLength(0); i++)
+            //rotating last row
+            row = m - height - 1;
+            col = width - 1;
+            for (int i = 0; i < width; i++, col++)
             {
-                for (int j = 0; j < result.GetLength(1); j++)
-                    Console.Write(result[i, j] + " ");
+                int currentValue = matrix[row, col];
+                int x, y;
+                GetCoords(width, height, row, col, rot, out x, out y);
+                result[x + height - 1, y + width - 1] = currentValue;
+            }
+
+            //rotating first column
+            row = height - 1;
+            col = width - 1;
+            for (int i = 0; i < height; i++, row++)
+            {
+                int currentValue = matrix[row, col];
+                int x, y;
+                GetCoords(width, height, row, col, rot, out x, out y);
+                result[x + height - 1, y + width - 1] = currentValue;
+            }
+
+            //rotation last column
+            row = height - 1;
+            col = n - width - 1;
+            for (int i = 0; i < height; i++, row++)
+            {
+                int currentValue = matrix[row, col];
+                int x, y;
+                GetCoords(width, height, row, col, rot, out x, out y);
+                result[x + height - 1, y + width - 1] = currentValue;
+            }
+        }
+
+        private static void GetCoords(int width, int height, int row, int col, int rot, out int x, out int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Print(int[,] matrix)
+        {
+            int m = matrix.GetLength(0), n = matrix.GetLength(1);
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(matrix[i, j] + " ");
                 Console.WriteLine();
             }
         }
