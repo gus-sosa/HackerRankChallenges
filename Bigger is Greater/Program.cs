602c0fae575c4b785d0eef50b3424a2d7a919c59
Gustavo Sosa <gcuervo@rdisoftware.com>
2017-06-01 16:17:54 -0500

    Temporal solution
    It works. It should improve the time execution. It runs out of time in some cases.

---
+++ Bigger is Greater/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bigger_is_Greater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                string word = Console.ReadLine();
+                string bestWord = null;
+                foreach (string permutation in GetPermutations(word))
+                    if (permutation.CompareTo(word) > 0 && (bestWord == null || bestWord.CompareTo(permutation) > 0))
+                        bestWord = permutation;
+                Console.WriteLine(bestWord ?? "no answer");
+            }
+        }
+
+        private static IEnumerable<string> GetPermutations(string word)
+        {
+            var marked = new bool[word.Length];
+            var builder = new StringBuilder();
+            foreach (string permutation in GetPermutations(word, builder, marked))
+                yield return permutation;
+        }
+
+        private static IEnumerable<string> GetPermutations(string word, StringBuilder builder, bool[] marked)
+        {
+            if (builder.Length == word.Length)
+                yield return builder.ToString();
+            else
+                for (int i = 0; i < marked.Length; i++)
+                    if (!marked[i])
+                    {
+                        marked[i] = true;
+                        builder.Append(word[i]);
+                        foreach (string permutation in GetPermutations(word, builder, marked))
+                            yield return permutation;
+                        marked[i] = false;
+                        builder.Remove(builder.Length - 1, 1);
+                    }
+        }
+    }
+}
