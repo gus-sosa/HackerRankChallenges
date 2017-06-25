5b0f64175ed87b351f1f3c5b8974aac846179f07
Gustavo Sosa <gcuervo@rdisoftware.com>
2017-06-25 16:04:17 -0500

    Temporal solution

--- IsFibo/Program.cs
+++ IsFibo/Program.cs
@@ -1,45 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace IsFibo
 {
     class Program
     {
-        static ulong[] table;
         static void Main(string[] args)
         {
-            PreCompute();
-
             int t = int.Parse(Console.ReadLine());
             ulong n;
             while (t-- > 0)
             {
                 n = ulong.Parse(Console.ReadLine());
-                Console.WriteLine(IsFibo(n) ? "IsFibo" : "IsNotFibo");
+                ulong tmp;
+                ulong _5nnPlus4 = 5 * n * n + 4;
+                ulong _5nnMinus4 = 5 * n * n - 4;
+                Console.WriteLine((ulong)((double)Math.Sqrt(_5nnMinus4)) == _5nnMinus4 || (ulong)((double)Math.Sqrt(_5nnPlus4)) == _5nnPlus4 ? "IsFibo" : "IsNotFibo");
             }
-        }
-
-        private static bool IsFibo(ulong n)
-        {
-            int index = Array.BinarySearch(table, n);
-            return index > 0;
-        }
-
-        private static void PreCompute()
-        {
-            var list = new List<ulong>() { 0, 1 };
-            ulong max = Convert.ToUInt64(Math.Pow(10, 10));
-            ulong a = 0;
-            ulong b = 1;
-            ulong current;
-            do
-            {
-                current = a + b;
-                list.Add(current);
-                a = b;
-                b = current;
-            } while (current <= max);
-            table = list.ToArray();
         }
     }
 }
