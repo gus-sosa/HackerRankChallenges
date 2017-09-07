17c1d4829033ad52a4d7c435ef157a5936241b3e
Unknown <g.sosacuervo1@gmail.com>
2017-09-07 11:48:59 -0500

    This solutions is right but it gives Timeout

--- RichieRich/Program.cs
+++ RichieRich/Program.cs
@@ -14,59 +14,32 @@ class Solution
     private static bool richieRich(StringBuilder s, int i, int j, int k)
     {
         if (k < 0) return false;
-        if (i >= j) return true;
-        if (s[i] == s[j]) return richieRich(s, i + 1, j - 1, k);
+        if (i > j) return true;
 
         int si = Convert.ToInt32(s[i]);
         int sj = Convert.ToInt32(s[j]);
 
-        int max = Math.Max(si, sj);
-        s[i] = s[j] = Convert.ToChar(max);
-        string sol1 = "";
-        if (richieRich(s, i + 1, j - 1, k - 1))
-            sol1 = s.ToString();
-
-        s[i] = s[j] = MAX_DIGIT;
-        string sol2 = "";
-        if (richieRich(s, i + 1, j - 1, k - 2))
-            sol2 = s.ToString();
-
-        if (string.IsNullOrEmpty(sol1) && string.IsNullOrEmpty(sol2))
+        if (s[i] != '9' && s[j] != '9')
         {
+            s[i] = s[j] = MAX_DIGIT;
+            if (richieRich(s, i + 1, j - 1, i == j ? k - 1 : k - 2))
+                return true;
             s[i] = Convert.ToChar(si);
             s[j] = Convert.ToChar(sj);
-            return false;
         }
 
-        if (Greater(sol1, sol2))
-            s = new StringBuilder(sol1);
+        if (s[i] == s[j])
+            return richieRich(s, i + 1, j - 1, k);
 
-        return true;
-    }
+        int max = Math.Max(si, sj);
+        s[i] = s[j] = Convert.ToChar(max);
+        if (richieRich(s, i + 1, j - 1, k - 1))
+            return true;
 
-    private static bool Greater(string sol1, string sol2)
-    {
-        int lengthSol1 = Length(sol1);
-        int lengthSol2 = Length(sol2);
-        if (lengthSol1 != lengthSol2)
-            return lengthSol1 > lengthSol2;
+        s[i] = Convert.ToChar(si);
+        s[j] = Convert.ToChar(sj);
 
-        for (int i = Math.Max(sol1.Length, sol2.Length) - 1, isol1 = 0, isol2 = 0; i < sol1.Length && i < sol2.Length; i++)
-        {
-            isol1 = sol1.Length - 1 - i;
-            isol2 = sol2.Length - 1 - i;
-            if (sol1[isol1] == sol2[isol2]) continue;
-            else return sol1[isol1] > sol2[isol2];
-        }
         return false;
-    }
-
-    private static int Length(string cad)
-    {
-        int i = 0;
-        while (i < cad.Length && cad[i] == '0')
-            i++;
-        return cad.Length - i;
     }
 
     static void Main(String[] args)
