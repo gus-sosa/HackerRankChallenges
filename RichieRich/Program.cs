343edfcf7d88c8d7d05ed1c4c6c413b484abf50c
Unknown <g.sosacuervo1@gmail.com>
2017-09-11 12:20:23 -0500

    Solution to problem
    https://www.hackerrank.com/challenges/richie-rich/problem

--- RichieRich/Program.cs
+++ RichieRich/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//#define LAUNCH_DEBUGGER
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 class Solution
@@ -7,43 +11,73 @@ class Solution
     const char MAX_DIGIT = '9';
     static string richieRich(string s, int n, int k)
     {
-        var parameter = new StringBuilder(s);
-        return richieRich(parameter, 0, s.Length - 1, k) ? parameter.ToString() : NO_VALUE;
-    }
+        var cad = new StringBuilder(s);
+        int[] result = MakeItPalindrome(cad);
+        int changesMade = result[0];
+        int[] posChanged = new int[result.Length - 1];
+        Array.Copy(result, 1, posChanged, 0, result.Length - 1);
 
-    private static bool richieRich(StringBuilder s, int i, int j, int k)
-    {
-        if (k < 0) return false;
-        if (i > j) return true;
+        if (k < changesMade)
+            return NO_VALUE;
+        if (k == changesMade)
+            cad.ToString();
 
-        int si = Convert.ToInt32(s[i]);
-        int sj = Convert.ToInt32(s[j]);
-
-        if (s[i] != '9' && s[j] != '9')
+        int i = 0, j = cad.Length - 1;
+        for (; i < j; i++, j--)
         {
-            s[i] = s[j] = MAX_DIGIT;
-            if (richieRich(s, i + 1, j - 1, i == j ? k - 1 : k - 2))
-                return true;
-            s[i] = Convert.ToChar(si);
-            s[j] = Convert.ToChar(sj);
+            if (cad[i] == MAX_DIGIT)
+                continue;
+
+            bool wasChanged = posChanged.Length > 0 && Array.BinarySearch(posChanged, i) >= 0;
+            if (wasChanged)
+            {
+                if (k < changesMade + 1)
+                    break;
+
+                changesMade++;
+            }
+            else
+            {
+                if (k < changesMade + 2)
+                    continue;
+
+                changesMade += 2;
+            }
+
+            cad[i] = cad[j] = MAX_DIGIT;
         }
 
-        if (s[i] == s[j])
-            return richieRich(s, i + 1, j - 1, k);
+        if (cad.Length % 2 != 0 && cad[cad.Length / 2] != MAX_DIGIT && k >= changesMade + 1)
+        {
+            cad[cad.Length / 2] = MAX_DIGIT;
+            changesMade++;
+        }
 
-        int max = Math.Max(si, sj);
-        s[i] = s[j] = Convert.ToChar(max);
-        if (richieRich(s, i + 1, j - 1, k - 1))
-            return true;
+        return cad.ToString();
+    }
 
-        s[i] = Convert.ToChar(si);
-        s[j] = Convert.ToChar(sj);
-
-        return false;
+    private static int[] MakeItPalindrome(StringBuilder s)
+    {
+        int changes = 0;
+        var result = new List<int>() { 0 };
+        for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            if (s[i] != s[j])
+            {
+                var iInt = Convert.ToInt32(s[i]);
+                var jInt = Convert.ToInt32(s[j]);
+                s[i] = s[j] = Convert.ToChar(Math.Max(iInt, jInt));
+                result.Add(i);
+                changes++;
+            }
+        result[0] = changes;
+        return result.ToArray();
     }
 
     static void Main(String[] args)
     {
+#if LAUNCH_DEBUGGER
+        Debugger.Launch();
+#endif
         string[] tokens = Console.ReadLine().Split();
         int n = Convert.ToInt32(tokens[0]);
         int k = Convert.ToInt32(tokens[1]);
