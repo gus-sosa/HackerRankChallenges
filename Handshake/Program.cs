2a85f58e63a02b5a2b510ab0e2bb3dbfd58a5d9f
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-09-25 22:38:46 -0500

    Solution to problem Handshake
    https://www.hackerrank.com/challenges/handshake/problem

---
+++ Handshake/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Handshake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                ulong n = ulong.Parse(Console.ReadLine());
+                Console.WriteLine(n * (n - 1) / 2);
+            }
+        }
+    }
+}
