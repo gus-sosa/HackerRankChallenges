cbe2113e39fea26c2b25956ee10b15424c95ee25
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-10-11 17:13:43 -0500

    Final accepted solution

--- ReverseGame/Program.cs
+++ ReverseGame/Program.cs
@@ -12,8 +12,8 @@ namespace ReverseGame
                 string[] tokens = Console.ReadLine().Split();
                 int n = int.Parse(tokens[0]);
                 int k = int.Parse(tokens[1]);
-                int half = n / 2 + (n % 2 == 0 ? 0 : 1);
-                Console.WriteLine(k >= half ? (2 * (n - k)) : (2 * (k + 1) - 1));
+                int half = n / 2;
+                Console.WriteLine(k >= half ? (2 * (n - 1 - k)) : (2 * k + 1));
             }
         }
     }
