7ba67490fcd5eaf726ee85a1810a7b1ab487bf92
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-10-12 13:47:50 -0500

    Solution to problem
    https://www.hackerrank.com/challenges/strange-grid/problem

---
+++ StrangeGrid/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StrangeGrid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            ulong r = ulong.Parse(tokens[0]) - 1;
+            ulong c = ulong.Parse(tokens[1]) - 1;
+            ulong sum = (ulong)(r % 2 == 0 ? 0 : 1);
+            ulong factor = r % 2 == 0 ? r : r - 1;
+            Console.WriteLine(5 * factor + 2 * c + sum);
+        }
+    }
+}
