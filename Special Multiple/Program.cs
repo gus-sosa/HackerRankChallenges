e7ff32581efe017aef926301ce885acb000f7d4a
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-10-16 17:08:38 -0500

    Solution to the problem Special Multiple
    https://www.hackerrank.com/challenges/special-multiple/problem

---
+++ Special Multiple/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Special_Multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetMul((ulong)n));
+            }
+        }
+
+        private static ulong GetMul(ulong n)
+        {
+            for (int i = 1; true; i++)
+            {
+                ulong nineRep = GetNineRepresentation(i);
+                if (nineRep % n == 0)
+                    return nineRep;
+            }
+        }
+
+        private static ulong GetNineRepresentation(int n)
+        {
+            return ulong.Parse(Convert.ToString(n, 2).Replace('1', '9'));
+        }
+    }
+}
