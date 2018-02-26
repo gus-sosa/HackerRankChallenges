13a09f379b29b21666ec0c2a48859f28005d3b0a
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-02-26 12:30:49 -0600

    Re-implementing the solution
    Following some of the notes in this link: http://theoryofprogramming.com/category/math/

--- Sherlock and Permutations/Program.cs
+++ Sherlock and Permutations/Program.cs
@@ -4,8 +4,7 @@ namespace Sherlock_and_Permutations
 {
     class Program
     {
-        const ulong mod1 = 1000000007;
-        const ulong mod2 = 1000000000000;
+        const ulong mod = 1000000007;
         const int maxLength = 2001;
         private static ulong[] fact = new ulong[maxLength];
         static void Main(string[] args)
@@ -16,18 +15,21 @@ namespace Sherlock_and_Permutations
             {
                 string[] tokens = Console.ReadLine().Split();
                 ulong n = ulong.Parse(tokens[0]);
-                ulong m = ulong.Parse(tokens[1]) - 1;
+                ulong m = ulong.Parse(tokens[1]);
                 Console.WriteLine(GetPermutations(n, m));
             }
         }
 
-        private static ulong GetPermutations(ulong n, ulong m) { return (fact[n + m] / (fact[n] * fact[m])) % mod1; }
+        private static ulong GetPermutations(ulong n, ulong m)
+        {
+            return (fact[n + m] / (fact[n] * fact[m])) % mod;
+        }
 
         private static void PreComputeFactorials()
         {
             fact[0] = fact[1] = 1;
             for (int i = 2; i < fact.Length; i++)
-                fact[i] = (Convert.ToUInt64(i) * fact[i - 1]) % mod2;
+                fact[i] = (Convert.ToUInt64(i) * fact[i - 1]) % mod;
         }
     }
 }
