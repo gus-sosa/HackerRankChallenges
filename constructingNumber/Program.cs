2c624a2eea7050e00aef1b8f3e26a0c35a2ad60b
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-25 17:07:50 -0500

    Solution to problem Constructing A Number
    https://www.hackerrank.com/challenges/constructing-a-number/problem

---
+++ constructingNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static string canConstruct(int[] a)
+    {
+        int sum = 0;
+        foreach (int num in a)
+        {
+            int n = num;
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+        }
+        return sum % 3 == 0 ? "Yes" : "No";
+    }
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            string result = canConstruct(a);
+            Console.WriteLine(result);
+        }
+    }
+}
