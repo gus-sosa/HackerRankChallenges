86184b9703d2b7ddee6b2d9e70c5a2483f45b9a9
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-04-10 01:06:35 -0500

    Final solution to problem Coinage
    https://www.hackerrank.com/challenges/coinage/problem

--- Coinage/Program.cs
+++ Coinage/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Coinage
 {
     class Program
     {
-        static int[] denominations = { 1, 2, 5, 10 };
-        static Dictionary<int[], ulong> map = new Dictionary<int[], ulong>();
         static void Main(string[] args)
         {
             int t = int.Parse(Console.ReadLine());
@@ -17,35 +12,18 @@ namespace Coinage
             {
                 int n = int.Parse(Console.ReadLine());
                 int[] denominationsCount = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                Console.WriteLine(GetCombinations(n, denominationsCount.Length - 1, denominationsCount));
+                int onelen = denominationsCount[0],
+                    twolen = denominationsCount[1],
+                    fivelen = denominationsCount[2],
+                    tenlen = denominationsCount[3];
+                int count = 0;
+                for (int ten = 0; ten <= tenlen && 10 * ten <= n; ten++)
+                    for (int five = 0; five <= fivelen && 10 * ten + 5 * five <= n; five++)
+                        for (int two = 0; two <= twolen && 10 * ten + 5 * five + 2 * two <= n; two++)
+                            if (n - (10 * ten + 5 * five + 2 * two) <= onelen)
+                                count++;
+                Console.WriteLine(count);
             }
-        }
-
-        private static ulong GetCombinations(int n, int currentPos, int[] denominationsCount)
-        {
-            if (n == 0)
-                return 1;
-
-            if (currentPos < 0)
-                return 0;
-
-            int[] newMap = new[] { n }.Concat(denominationsCount).ToArray();
-            if (map.ContainsKey(newMap))
-                return map[newMap];
-
-            ulong result = 0;
-            var newDenominationsCount = (int[])denominationsCount.Clone();
-            for (int denominationCount = Math.Min(n / denominations[currentPos], denominationsCount[currentPos]);
-                denominationCount >= 0; denominationCount--)
-                if (denominations[currentPos] * denominationCount <= n)
-                {
-                    newDenominationsCount[currentPos] -= denominationCount;
-                    result += GetCombinations(n - denominations[currentPos] * denominationCount, currentPos - 1,
-                        newDenominationsCount);
-                    newDenominationsCount[currentPos] += denominationCount;
-                }
-
-            return map[newMap] = result;
         }
     }
 }
