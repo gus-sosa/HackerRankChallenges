ce2b845ca1877ed45c4d5539a34decc447e72f91
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-04-11 01:19:25 -0500

    Solution to problem Building List
    https://www.hackerrank.com/challenges/building-a-list/problem

---
+++ BuildingList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuildingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int n = int.Parse(Console.ReadLine());
+                char[] set = Console.ReadLine().ToCharArray();
+
+                var pivote = (int)Math.Pow(2, n) - 1;
+                var orderSet = new SortedSet<string>();
+                while (pivote > 0)
+                    orderSet.Add(GetCad(set, pivote--));
+
+                foreach (string s in orderSet)
+                    Console.WriteLine(s);
+            }
+        }
+
+        private static string GetCad(char[] set, int pivote)
+        {
+            string r = "";
+            foreach (char t in set)
+            {
+                if ((pivote & 1) == 1)
+                    r += t;
+                pivote >>= 1;
+            }
+            return r;
+        }
+    }
+}
