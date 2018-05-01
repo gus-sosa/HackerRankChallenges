c87bc50a517646e38c308aa64d4a11fdadc4152e
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-05-01 13:35:11 -0500

    Sacaffolding of project Largest Permutation
    https://www.hackerrank.com/challenges/largest-permutation/problem

---
+++ LargestPermutation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static int[] largestPermutation(int k, int[] arr)
+    {
+        // Complete this function
+    }
+
+    static void Main(String[] args)
+    {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        int[] result = largestPermutation(k, arr);
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
