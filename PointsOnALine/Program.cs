03ce373945fa84b54303cacaacf8eddf7d38cc00
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-05-15 11:17:06 -0500

    Solution to problem PointsOnALine
    https://www.hackerrank.com/challenges/points-on-a-line/problem

---
+++ PointsOnALine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        bool validPoints = true, validHorizontalPoints = true, validVerticalPoints = true;
+        string[] token = Console.ReadLine().Split();
+        int xPivot = int.Parse(token[0]), yPivot = int.Parse(token[1]);
+        for (int nItr = 1; nItr < n && validPoints; nItr++)
+        {
+            token = Console.ReadLine().Split();
+            int x = Convert.ToInt32(token[0]);
+            int y = Convert.ToInt32(token[1]);
+
+            validVerticalPoints = validVerticalPoints && x == xPivot;
+            validHorizontalPoints = validHorizontalPoints && y == yPivot;
+            validPoints = validHorizontalPoints || validVerticalPoints;
+        }
+
+        Console.WriteLine(validPoints ? "YES" : "NO");
+    }
+}
