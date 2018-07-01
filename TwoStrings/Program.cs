da5371fdde3d8f848e9b91ea130e77b10a6dbe36
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-07-01 17:25:06 -0500

    Solution to problem Two Strings
    https://www.hackerrank.com/challenges/two-strings/problem?h_l=playlist&slugs%5B%5D=interview&slugs%5B%5D=interview-preparation-kit&slugs%5B%5D=dictionaries-hashmaps

---
+++ TwoStrings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Solution
+{
+    private static readonly char[] alphabetArray = "abcdefghijklmnopqrstuvwxyz".ToArray();
+    const string YES = "YES";
+    const string NO = "NO";
+    // Complete the twoStrings function below.
+    static string twoStrings(string s1, string s2)
+    {
+        var hash1 = new HashSet<char>();
+        var hash2 = new HashSet<char>();
+        AddToHash(s1, hash1);
+        AddToHash(s2, hash2);
+        foreach (char c in alphabetArray)
+            if (hash1.Contains(c) && hash2.Contains(c))
+                return YES;
+        return NO;
+    }
+
+    private static void AddToHash(string cad, HashSet<char> hash)
+    {
+        foreach (var item in cad)
+            hash.Add(item);
+    }
+
+    static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = twoStrings(s1, s2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
