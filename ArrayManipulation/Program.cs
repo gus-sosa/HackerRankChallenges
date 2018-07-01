0afca8054fcaa08816c3856e1b692bc60f30d9c4
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-07-01 12:11:25 -0500

    Project creation

---
+++ ArrayManipulation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries)
+    {
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nm = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nm[0]);
+
+        int m = Convert.ToInt32(nm[1]);
+
+        int[][] queries = new int[m][];
+
+        for (int i = 0; i < m; i++)
+        {
+            queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
