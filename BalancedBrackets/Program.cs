b9bd0c0518f84eabf978c2f2f37e1deeee706f21
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-07-02 12:19:01 -0500

    Solution to problem Balanced Brackets
    https://www.hackerrank.com/challenges/balanced-brackets/problem?h_l=playlist&slugs%5B%5D=interview&slugs%5B%5D=interview-preparation-kit&slugs%5B%5D=stacks-queues

---
+++ BalancedBrackets/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Solution
+{
+    const string YES = "YES";
+    const string NO = "NO";
+    static readonly char[] openBrackets = new[] { '(', '[', '{' };
+    static readonly Dictionary<char, char> pairBrackets = new Dictionary<char, char>()
+    {
+        ['('] = ')',
+        ['['] = ']',
+        ['{'] = '}'
+    };
+    // Complete the isBalanced function below.
+    static string isBalanced(string s)
+    {
+        var stack = new Stack<char>();
+        bool flag = true;
+        foreach (char c in s)
+        {
+            if (openBrackets.Contains(c)) stack.Push(c);
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    flag = false;
+                    break;
+                }
+
+                char top = pairBrackets[stack.Pop()];
+                if (top != c)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            flag = stack.Count == 0;
+        }
+        return flag ? YES : NO;
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = isBalanced(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
