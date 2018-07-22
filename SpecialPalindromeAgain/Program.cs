0cd008279641b722d06f5f356e951dff17e5a8c9
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-07-22 02:55:26 -0500

    Trying with dictionary. Timeout and wrong answer.

--- SpecialPalindromeAgain/Program.cs
+++ SpecialPalindromeAgain/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 class Solution
@@ -7,27 +8,25 @@ class Solution
     // Complete the substrCount function below.
     static long substrCount(string s)
     {
-        var matrix = new char[s.Length, s.Length];
+        var map = new Dictionary<int, Dictionary<int, char>>();
         long count = 0;
-
+        //Debugger.Launch();
         for (int i = 0; i < s.Length; i++)
         {
             ++count;
-            matrix[i, i] = s[i];
+            AddKeyValue(map, Tuple.Create(i, i), s[i]);
             if (i + 1 < s.Length && s[i] == s[i + 1])
             {
-                matrix[i, i + 1] = s[i];
+                AddKeyValue(map, Tuple.Create(i, i + 1), s[i]);
                 ++count;
             }
             if (i + 2 < s.Length && s[i] == s[i + 2])
             {
-                matrix[i, i + 2] = s[i];
-                matrix[i + 2, i] = s[i + 1];
+                AddKeyValue(map, Tuple.Create(i, i + 2), s[i]);
+                AddKeyValue(map, Tuple.Create(i + 2, i), s[i + 1]);
                 ++count;
             }
         }
-
-        Debugger.Launch();
 
         for (int l = 4; l <= s.Length; l++)
             for (int i = 0; i + l - 1 < s.Length; i += l)
@@ -37,18 +36,45 @@ class Solution
                     iEndHead = middleIndex - 1,
                     iStartTail = l % 2 == 0 ? middleIndex : middleIndex + 1,
                     iEndTail = i + l - 1;
-                if (matrix[iStartHead, iEndHead] != NULL_CHARACTER && matrix[iStartHead, iEndHead] == matrix[iStartTail, iEndTail] &&
-                    matrix[iEndHead, iStartHead] == NULL_CHARACTER && matrix[iEndHead, iStartHead] == matrix[iEndTail, iStartTail])
+                var head = Tuple.Create(iStartHead, iEndHead);
+                var tail = Tuple.Create(iStartTail, iEndTail);
+                var headReverse = Tuple.Create(iEndHead, iStartHead);
+                var tailReverse = Tuple.Create(iEndTail, iStartTail);
+                if (ContainKey(map, head) && GetValue(map, head) == GetValue(map, tail) &&
+                      GetValue(map, headReverse) == NULL_CHARACTER && GetValue(map, headReverse) == GetValue(map, tailReverse))
                 {
                     ++count;
-                    matrix[i, i + l - 1] = s[i];
+                    AddKeyValue(map, Tuple.Create(i, i + l - 1), s[i]);
                     if (l % 2 != 0 && s[middleIndex] != s[i])
-                        matrix[i + l - 1, i] = s[middleIndex];
+                        AddKeyValue(map, Tuple.Create(i + l - 1, i), s[middleIndex]);
                 }
             }
 
         return count;
     }
+
+    private static char GetValue(Dictionary<int, Dictionary<int, char>> map, Tuple<int, int> key)
+    {
+        var dictionary = map[key.Item1];
+        if (dictionary != null && dictionary.ContainsKey(key.Item2))
+            return dictionary[key.Item2];
+        return NULL_CHARACTER;
+    }
+
+    private static bool ContainKey(Dictionary<int, Dictionary<int, char>> map, Tuple<int, int> key)
+    {
+        var dictionary = map[key.Item1];
+        return dictionary != null && dictionary.ContainsKey(key.Item2);
+    }
+
+    private static void AddKeyValue(Dictionary<int, Dictionary<int, char>> map, Tuple<int, int> key, char value)
+    {
+        if (!map.ContainsKey(key.Item1))
+            map[key.Item1] = new Dictionary<int, char>();
+        map[key.Item1][key.Item2] = value;
+    }
+
+    private static char GetCharacter(Dictionary<int[], char> map, int[] positions) { return map.ContainsKey(positions) ? map[positions] : NULL_CHARACTER; }
 
     static void Main(string[] args)
     {
