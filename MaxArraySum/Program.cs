210f4637979841d8d8bdfc85479e267ca889792a
Unknown <g.sosacuervo1@gmail.com>
2018-10-24 18:27:47 -0500

    Project creation
    https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming

---
+++ MaxArraySum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Solution
+{
+
+    // Complete the maxSubsetSum function below.
+    private static int maxSubsetSum(int[] arr)
+    {
+        return 0;
+    }
+
+    private static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine());
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' ') , arrTemp => Convert.ToInt32(arrTemp));
+        var res = maxSubsetSum(arr);
+        Console.WriteLine(res);
+    }
+}
