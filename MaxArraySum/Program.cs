d34484e008c973450992e594519e74459cda9094
Unknown <g.sosacuervo1@gmail.com>
2018-10-24 21:06:05 -0500

    Temporal solution
    It is not working because it is not taking the second max correctly

--- MaxArraySum/Program.cs
+++ MaxArraySum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 internal class Solution
 {
@@ -6,7 +8,48 @@ internal class Solution
     // Complete the maxSubsetSum function below.
     private static int maxSubsetSum(int[] arr)
     {
-        return 0;
+        Debugger.Launch();
+
+        if (arr.Length == 1)
+            return arr[0];
+
+        int prevPrevMax = arr[0], prevMax = int.MinValue;
+        var prevLastPosTaken = false;
+
+        Tuple<int , bool> newMax = computeSubsetSum(arr[1] , prevPrevMax , prevMax , prevLastPosTaken);
+        prevMax = newMax.Item1;
+        prevLastPosTaken = newMax.Item2;
+
+        for (var i = 2 ; i < arr.Length ; i++)
+        {
+            newMax = computeSubsetSum(arr[i] , prevPrevMax , prevMax , prevLastPosTaken);
+            prevPrevMax = prevMax;
+            prevMax = newMax.Item1;
+            prevLastPosTaken = newMax.Item2;
+        }
+
+        return prevMax;
+    }
+
+    private static Tuple<int , bool> computeSubsetSum(int num , int prevPrevMax , int prevMax , bool prevLastPosTaken)
+    {
+        var taken = false;
+        var max = 0;
+        var currentPrevSum = num + prevMax;
+        var currentPrevPrevSum = num + prevPrevMax;
+
+        if (prevLastPosTaken)
+        {
+            max = Enumerable.Max(new int[] { num , prevMax , currentPrevPrevSum });
+            taken = max == num || max == currentPrevPrevSum;
+        }
+        else
+        {
+            max = Enumerable.Max(new int[] { num , prevMax , currentPrevSum , currentPrevPrevSum , prevMax , prevPrevMax });
+            taken = max == num || max == currentPrevSum || max == currentPrevPrevSum;
+        }
+
+        return Tuple.Create(max , taken);
     }
 
     private static void Main(string[] args)
