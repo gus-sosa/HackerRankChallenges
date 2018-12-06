c13c205b3b85d71f29a7caca27f54dbe86835ecc
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-12-06 14:40:17 -0600

    Solution to problem Counting Valleys
    https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+internal class Solution
+{
+    protected internal const char DOWN = 'D';
+
+    // Complete the countingValleys function below.
+    private static int countingValleys(int n, string s)
+    {
+        int currentLevel = 0, numValleys = 0;
+        foreach (var step in s)
+            if (step == DOWN)
+            {
+                if (currentLevel == 0)
+                    numValleys++;
+                currentLevel--;
+            }
+            else currentLevel++;
+        return numValleys;
+    }
+
+    private static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine());
+        var s = Console.ReadLine();
+        var result = countingValleys(n, s);
+        Console.WriteLine(result);
+    }
+}
