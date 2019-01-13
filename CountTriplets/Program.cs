1e44e3274bd8880832d2b4ba0665c2458fe5f196
Gus <g.sosacuervo1@gmail.com>
2019-01-12 18:28:45 -0600

    Some improvements
    #Refactorizations
    #Fixed mistype
    #Fixed IsPowerOfRatio, then two failed test cases passed

--- CountTriplets/Program.cs
+++ CountTriplets/Program.cs
@@ -1,28 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 class Solution
 {
-    private static long _countTriplets;
-    private static Dictionary<long, long> _tuples;
-    private static Dictionary<long, long> _singles;
+    private static long _countTriplets = 0;
+    private static Dictionary<long, long> _tuples = new Dictionary<long, long>();
+    private static Dictionary<long, long> _singles = new Dictionary<long, long>();
 
 
     // Complete the countTriplets function below.
     static long countTriplets(long[] arr, long r)
     {
-        Debugger.Launch();
-        _countTriplets = 0;
-        _tuples = new Dictionary<long, long>();
-        _singles = new Dictionary<long, long>();
-
+        //Debugger.Launch();
         foreach (var currentNum in arr)
             if (IsPowerOfRatio(currentNum, r))
             {
-                AggregateTriplets(currentNum, r);
-                AggregateDoubles(currentNum, r);
                 AggregateSingles(currentNum);
+                AggregateDoubles(currentNum, r);
+                AggregateTriplets(currentNum, r);
             }
 
         return _countTriplets;
@@ -39,35 +34,31 @@ class Solution
     {
         var previousInProgression = PreviousInProgression(currentNum, ratio);
         if (_singles.ContainsKey(previousInProgression))
-            AddTupletes(currentNum, _singles[previousInProgression]);
+            AddTuplets(currentNum, _singles[previousInProgression]);
     }
 
-    private static void AddTupletes(long endTuplete, long cant)
+    private static void AddTuplets(long endTuplete, long cant)
     {
         if (!_tuples.ContainsKey(endTuplete))
             _tuples[endTuplete] = 0;
         _tuples[endTuplete] += cant;
     }
 
-    private static void AggregateTriplets(long num, long ratio) => _countTriplets += (_tuples.ContainsKey(PreviousInProgression(num, ratio)) ? _tuples[PreviousInProgression(num, ratio)] : 0);
+    private static void AggregateTriplets(long num, long ratio) => _countTriplets += (_tuples.ContainsKey(PreviousInProgression(num, ratio)) ?
+        _tuples[PreviousInProgression(num, ratio)] : 0);
 
     private static long PreviousInProgression(long num, long ratio) => num / ratio;
 
-    private static bool IsPowerOfRatio(long num, long ratio) => (ratio == 1 && num == 1) || (num == 1 && ratio > 1) ||
+    private static bool IsPowerOfRatio(long num, long ratio) => ratio == 1 || num == 1 ||
         (num % ratio == 0 && long.TryParse(Math.Log(num, ratio).ToString(), out _));
 
     static void Main(string[] args)
     {
         string[] nr = Console.ReadLine().Split(' ');
-
         long n = Convert.ToInt64(nr[0]);
-
         long r = Convert.ToInt64(nr[1]);
-
-        long[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt64(arrTemp))
-        ;
+        long[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt64(arrTemp));
         long ans = countTriplets(arr, r);
-
         Console.WriteLine(ans);
     }
 }
