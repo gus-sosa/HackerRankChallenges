d488de7293749ab5c64a811ebbf7044dba1f19a2
Gus <gcuervo@gtreasury.com>
2019-08-16 11:05:35 -0500

    Final and accepted solution to count triplets

--- CountTriplets/Program.cs
+++ CountTriplets/Program.cs
@@ -1,64 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 class Solution
 {
-    private static long _countTriplets = 0;
-    private static Dictionary<long, long> _tuples = new Dictionary<long, long>();
-    private static Dictionary<long, long> _singles = new Dictionary<long, long>();
+  // Complete the countTriplets function below.
+  static long countTriplets(long[] arr, long ratio) {
+    //Debugger.Launch();
+    Dictionary<long, long> js = new Dictionary<long, long>(), ks = new Dictionary<long, long>();
+    long count = 0;
 
-
-    // Complete the countTriplets function below.
-    static long countTriplets(long[] arr, long r)
-    {
-        //Debugger.Launch();
-        foreach (var currentNum in arr)
-            if (IsPowerOfRatio(currentNum, r))
-            {
-                AggregateSingles(currentNum);
-                AggregateDoubles(currentNum, r);
-                AggregateTriplets(currentNum, r);
-            }
-
-        return _countTriplets;
+    foreach (var value in arr) {
+      if (!ks.ContainsKey(value)) {
+        ks[value] = 0;
+      }
+      count += ks[value];
+      if (!js.ContainsKey(value)) {
+        js[value] = 0;
+      }
+      if (!ks.ContainsKey(value * ratio)) {
+        ks[value * ratio] = 0;
+      }
+      ks[value * ratio] += js[value];
+      if (!js.ContainsKey(value * ratio)) {
+        js[value * ratio] = 0;
+      }
+      ++js[value * ratio];
     }
 
-    private static void AggregateSingles(long num)
-    {
-        if (!_singles.ContainsKey(num))
-            _singles[num] = 0;
-        ++_singles[num];
-    }
+    return count;
+  }
 
-    private static void AggregateDoubles(long currentNum, long ratio)
-    {
-        var previousInProgression = PreviousInProgression(currentNum, ratio);
-        if (_singles.ContainsKey(previousInProgression))
-            AddTuplets(currentNum, _singles[previousInProgression]);
-    }
-
-    private static void AddTuplets(long endTuplete, long cant)
-    {
-        if (!_tuples.ContainsKey(endTuplete))
-            _tuples[endTuplete] = 0;
-        _tuples[endTuplete] += cant;
-    }
-
-    private static void AggregateTriplets(long num, long ratio) => _countTriplets += (_tuples.ContainsKey(PreviousInProgression(num, ratio)) ?
-        _tuples[PreviousInProgression(num, ratio)] : 0);
-
-    private static long PreviousInProgression(long num, long ratio) => num / ratio;
-
-    private static bool IsPowerOfRatio(long num, long ratio) => ratio == 1 || num == 1 ||
-        (num % ratio == 0 && long.TryParse(Math.Log(num, ratio).ToString(), out _));
-
-    static void Main(string[] args)
-    {
-        string[] nr = Console.ReadLine().Split(' ');
-        long n = Convert.ToInt64(nr[0]);
-        long r = Convert.ToInt64(nr[1]);
-        long[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt64(arrTemp));
-        long ans = countTriplets(arr, r);
-        Console.WriteLine(ans);
-    }
+  static void Main(string[] args) {
+    string[] nr = Console.ReadLine().Split(' ');
+    long n = Convert.ToInt64(nr[0]);
+    long r = Convert.ToInt64(nr[1]);
+    long[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt64(arrTemp));
+    long ans = countTriplets(arr, r);
+    Console.WriteLine(ans);
+  }
 }
